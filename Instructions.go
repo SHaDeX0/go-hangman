b391881ea077ee49b8e7c5032dbba094a32853e7
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2023-05-18 20:51:55 +0530

    add game functionality

--- Instructions.go
+++ Instructions.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func help() {
-	fmt.Println("Help meeeeeeee!")
+	fmt.Println("I will pick a random country name.\n" +
+		"That is the Magic Country which you will have to guess.\n" +
+		"You will have " + strconv.Itoa(Lives) + " lives to completely guessCountry the Magic Country.\n" +
+		"If you guessCountry a letter which is not present in the Magic Country, you will lose a life.\n" +
+		"When you lose all your lives or guessCountry the Magic Country name, the game will be over.\n" +
+		"Now, let's get started!\n\n")
 }
--- Play.go
+++ Play.go
@@ -3,30 +3,89 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 )
 
 var (
-	// country that the player has to guess fully to win
+	countries    = [...]string{"Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"}
+	gameOver     = false
+	Lives        = 10
 	magicCountry string
 	guessLetter  string
-	guess        = ""
-	gameOver     = false
+	guessCountry string
 )
 
 func play() {
-	magicCountry = Countries[rand.Intn(len(Countries))]
+	magicCountry = strings.ToLower(countries[rand.Intn(len(countries))])
 	initGuess()
 
 	for !gameOver {
-		_, err := fmt.Scanln(guessLetter)
+		fmt.Println("\n\nLives remaining: ", Lives)
+		fmt.Println("Country guess: " + guessCountry)
+		fmt.Println("\n\nEnter your guess letter: ")
+		_, err := fmt.Scanln(&guessLetter)
 		if err != nil {
 			fmt.Println(InvalidInput)
-			return
+			continue
 		}
 
+		if 1 < len(guessLetter) {
+			fmt.Println("Enter just a single letter!")
+			continue
+		}
+
+		if !(strings.Contains(magicCountry, guessLetter)) {
+			fmt.Println("Wrong Guess!")
+			Lives--
+			checkForLose()
+			continue
+		}
+
+		modifyGuess()
+		checkForWin()
+	}
+
+	if 0 == Lives {
+		fmt.Println("You lost! The Magic Country name was: " + magicCountry)
+	} else {
+		fmt.Println("Congratulations! You guessed the Magic Country: " + magicCountry)
+	}
+}
+
+func checkForLose() {
+	if 0 == Lives {
+		gameOver = true
+	}
+}
+
+func checkForWin() {
+	win := true
+	for i := 0; i < len(magicCountry); i++ {
+		if guessCountry[i] != magicCountry[i] {
+			win = false
+			break
+		}
+	}
+	if win {
+		gameOver = true
 	}
 }
 
 func initGuess() {
-	// TODO: Initialize guess here using the length of magicCountry
+	// Initialize guessCountry here using the length of magicCountry
+	for i := 0; i < len(magicCountry); i++ {
+		if 32 == magicCountry[i] {
+			guessCountry += " "
+			continue
+		}
+		guessCountry += "_"
+	}
+}
+
+func modifyGuess() {
+	for i := 0; i < len(magicCountry); i++ {
+		if magicCountry[i] == guessLetter[0] {
+			guessCountry = guessCountry[:i] + guessLetter + guessCountry[i+1:]
+		}
+	}
 }

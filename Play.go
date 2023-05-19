c81a7b261cfa63bc60668b5df87b047a9ef6ca54
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2023-05-19 19:00:22 +0530

    fix game re-play and optimize

--- Play.go
+++ Play.go
@@ -8,16 +8,16 @@ import (
 
 var (
 	countries    = [...]string{"Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"}
-	gameOver     = false
-	Lives        = 10
+	gameOver     bool
+	Lives        int
 	magicCountry string
 	guessLetter  string
 	guessCountry string
+	wrongGuesses string
 )
 
 func play() {
-	magicCountry = strings.ToLower(countries[rand.Intn(len(countries))])
-	initGuess()
+	initPlay()
 
 	for !gameOver {
 		fmt.Println("\n\nLives remaining: ", Lives)
@@ -35,8 +35,7 @@ func play() {
 		}
 
 		if !(strings.Contains(magicCountry, guessLetter)) {
-			fmt.Println("Wrong Guess!")
-			Lives--
+			checkForWrongGuesses()
 			checkForLose()
 			continue
 		}
@@ -50,6 +49,16 @@ func play() {
 	} else {
 		fmt.Println("Congratulations! You guessed the Magic Country: " + magicCountry)
 	}
+}
+
+func checkForWrongGuesses() {
+	if strings.Contains(wrongGuesses, guessLetter) {
+		fmt.Println("You already guessed this letter, and it's wrong!")
+		return
+	}
+	fmt.Println("Wrong guess!")
+	wrongGuesses += guessLetter
+	Lives--
 }
 
 func checkForLose() {
@@ -71,7 +80,13 @@ func checkForWin() {
 	}
 }
 
-func initGuess() {
+func initPlay() {
+	fmt.Println("Countries length: ", len(countries))
+	magicCountry = strings.ToLower(countries[rand.Intn(len(countries))])
+	gameOver = false
+	guessCountry = ""
+	wrongGuesses = ""
+	Lives = 10
 	// Initialize guessCountry here using the length of magicCountry
 	for i := 0; i < len(magicCountry); i++ {
 		if 32 == magicCountry[i] {
